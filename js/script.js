82055a5e90b19076dd727b3c9c6924eba336a2c9
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2021-09-18 17:42:50 +0600

    design doesn't complete yet

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function title_changer(text){
+    document.title = text + "| CRAFT LANCE";
+};
+
+// function click_not_load(e){
+//     e.preventDefault();
+// }
+
+// click_not_load = (e) => {
+//      e.preventDefault();
+// }
